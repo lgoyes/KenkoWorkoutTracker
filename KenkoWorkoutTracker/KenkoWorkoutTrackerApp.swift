1e166b39c3d811ed591cd7c72b7c6c2ea769cddd
Luis David Goyes Garces <luis.goyes117@gmail.com>
2025-04-24 12:14:08 -0500

    Initial Commit

---
+++ KenkoWorkoutTracker/KenkoWorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KenkoWorkoutTrackerApp.swift
+//  KenkoWorkoutTracker
+//
+//  Created by Luis David Goyes Garces on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct KenkoWorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KenkoWorkoutTrackerTests/KenkoWorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  KenkoWorkoutTrackerTests.swift
+//  KenkoWorkoutTrackerTests
+//
+//  Created by Luis David Goyes Garces on 24/4/25.
+//
+
+import Testing
+@testable import KenkoWorkoutTracker
+
+struct KenkoWorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
